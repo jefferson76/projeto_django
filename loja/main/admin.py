ba88b89cf7ae186581c930057125adc1fc3233c7
jefferson pereira <jefferson.pereira76@gmail.com>
2023-06-21 11:22:36 -0300

    Configurando a urls.py, views.py e templates

--- loja/main/admin.py
+++ loja/main/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from.models import Category,Marca,Color,Size,Product
+from .models import Category, Marca, Color, Size, Product, ProductAttribute
 
 admin.site.register(Category)
 admin.site.register(Marca)
 admin.site.register(Color)
 admin.site.register(Size)
-admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'marca', 'color', 'size', 'status')
+    list_editable = ('status',)
+    admin.site.register(Product)   #, ProductAdmin)
+
+
+class ProductAttributeAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'product', 'price', 'color', 'size')
+    admin.site.register(ProductAttribute)   #, ProductAttributeAdmin)
--- loja/main/models.py
+++ loja/main/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+# from email.policy import default
+# from django.db import models
+
+
+class Banner(models.Model):
+
+    img = models.CharField(max_length=250)
+    alt_text = models.CharField(max_length=300)
+
 
 class Category(models.Model):
+
     title = models.CharField(max_length=180)
     image = models.ImageField(upload_to="cat_imgs/")
 
@@ -9,38 +19,54 @@ class Category(models.Model):
 
 
 class Marca(models.Model):
+
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to="marca_imgs/")
 
     def __str__(self):
-        return self.title
-    
+        return self.title    
 
-class Color(models.Model):
+
+class Color(models.Model):  
+
     title = models.CharField(max_length=100)
     color_codigo = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.title
-    
+        return self.title  
+
+
 class Size(models.Model):
+
     title = models.CharField(max_length=100)
+
     def __str__(self):
         return self.title
 
+
 class Product(models.Model):
+
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="product_imgs/")
     slug = models.CharField(max_length=400)
     detail = models.TextField()
     specs = models.TextField()
-    price = models.PositiveBigIntegerField()
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
 
-
     def __str__(self):
         return self.title
+    
+
+class ProductAttribute(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product.title
